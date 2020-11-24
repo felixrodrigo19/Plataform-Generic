447711bce1391f71f594b813d985a5b765f8a6af
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2020-11-23 21:37:29 -0300

    Initial commit

---
+++ Assets/Scripts/MySystemPause.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySystemPause : MonoBehaviour
+{
+    public static bool gameIsPaused;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            gameIsPaused = !gameIsPaused;
+            Pause();
+        }
+
+        void Pause()
+        {
+            if (gameIsPaused)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/UIMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+
+    public void ExitGame()
+    {
+        EditorApplication.Exit(0);
+    }
+}
